df5491056db8890ccdab871da2571f15dd50dd2e
Ronald Tse <ronald.tse@ribose.com>
2025-06-09 13:48:52 +0800

    fix: only enable opal-rspec inside opal tests

--- spec/parslet/position_spec.rb
+++ spec/parslet/position_spec.rb
@@ -3,12 +3,12 @@
 require 'spec_helper'
 
 describe Parslet::Position do
-  slet(:position) { described_class.new('öäüö', 4) }
+  let(:position) { described_class.new('öäüö', 4) }
 
   it 'should have a charpos of 2' do
-    position.charpos.should == 2
+    expect(position.charpos).to eq(2)
   end
   it 'should have a bytepos of 4' do
-    position.bytepos.should == 4
+    expect(position.bytepos).to eq(4)
   end
 end
